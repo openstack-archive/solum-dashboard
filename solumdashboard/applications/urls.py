0f4b2f50d6e7bca2541ca71c045b5588b9c8a2ec
zhurong <aaronzhu1121@gmail.com>
2016-09-11 11:37:00 +0000

    Delete the unused url
    Delete the unused url, this caused can not find the LogView.
    
    Change-Id: Ia2b3a0cb6571f41808f606d7fee4e61b16b36cd0

--- solumdashboard/applications/urls.py
+++ solumdashboard/applications/urls.py
@@ -33,8 +33,6 @@ urlpatterns = patterns(
         name='launch'),
     url(r'^detail/(?P<application_id>[^/]+)$',
         views.DetailView.as_view(), name='detail'),
-    url(r'^log/(?P<application_id>[^/]+)$', views.LogView.as_view(),
-        name='log'),
     url(r'^scale/(?P<application_id>[^/]+)$', views.ScaleView.as_view(),
         name='scale'),
     url(r'^update/(?P<application_id>[^/]+)$', views.UpdateView.as_view(),
